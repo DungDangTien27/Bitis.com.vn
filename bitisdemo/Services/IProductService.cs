c5af04db23d1283c49c3365c21433518ae4edfe8
DungDangTien27 <91462172+DungDangTien27@users.noreply.github.com>
2021-11-18 18:38:35 +0700

    Add files via upload

---
+++ bitisdemo/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using bitis.mode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bitisdemo.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(int id);
+
+        Task<Product> UpdateProduct(Product updatedProduct);
+    }
+}
---
+++ bitisdemo/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using bitis.mode;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace bitisdemo.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient httpClient;
+        public ProductService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await httpClient.GetJsonAsync<Product>($"api/products/{id}");
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await httpClient.GetJsonAsync<Product[]>("api/products");
+        }
+
+        public async Task<Product> UpdateProduct(Product updatedProduct)
+        {
+            return await httpClient.PutJsonAsync<Product>("api/products", updatedProduct);
+        }
+
+    }
+}
